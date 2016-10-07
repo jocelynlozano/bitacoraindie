1596724268ad937fc92a74acde879fe2cd8ff3b1
jocelynlozano <jocelynlozanocarbajal@gmail.com>
2016-10-07 09:29:58 -0500

    opcion de ocultar y mostrar completa

--- js/main.js
+++ js/main.js
@@ -1,60 +1,66 @@
-window.addEventListener("load",main);
-
+window.addEventListener("load",function(){
+  swal ({ 
+  title: "Bitacora" ,   
+  text: "Comparte textos, citas, imagenes, pensamientos y mucho más con tus amigos en un espacio seguro" ,   
+  timer: 3000 ,  
+  showConfirmButton: false  
+});
+  var caja=document.getElementById("caja");
+  caja.style.display="none";
+  var ocultatePrimero=document.getElementById("ocultatePrimero"); 
+  var tasksContainer=document.getElementById("tasks-container");
+  ocultatePrimero.style.display="none";
+  tasksContainer.style.display="none";
+  var boton=document.getElementById("boton");
+  var texto=document.getElementById("texto");
+  texto.addEventListener("click",aparecer);
+  boton.addEventListener("click",listas);
+    function listas(){
+      caja.style.display="block";
+    }
+    function aparecer(){
+    ocultatePrimero.style.display="block";
+    tasksContainer.style.display="block";
+  }
+});
  
- swal ({ 
- 		title: "Bitacora" ,   
- 		text: "Comparte textos, citas, imagenes, pensamientos y mucho más con tus amigos en un espacio seguro" ,   
- 		timer: 3000 ,  
- 		showConfirmButton: false  
- });
-
-function Mostrar(){
-  document.getElementById("caja").style.display = "block"
-}
-function Ocultar(){
-  document.getElementById("caja").style.display = "none"
-}
-function mostrarOcultar(){
-  var caja = document.getElementById("caja")
-  if(caja.style.display == "none"){
-    Mostrar();
-    document.getElementById("boton").value = "Ocultar"
-  } else {
-    Ocultar();
-    document.getElementById("boton").value = "Publicar"
-  }
-}
 
 
-//Funcion que se ejecuta cuando termina de cargar
-function main() {
-  var addTaskButton = document.getElementById("add-task");
-  addTaskButton.addEventListener("click",onClickAddTask);
-}
 
-function onClickAddTask(e) {
-  e.preventDefault();
-  var text = getTextAreaValue();
-  if (text != "" && text.length <= 140) {
-    var newTask = createTask(text);
-    addTaskToContainer(newTask);
-  } else {
-    alert("Ingresa texto entre 0 y 140 caracteres");
-  }
-}
 
-function getTextAreaValue() {
-  var textarea = document.querySelector(".add-tasks textarea");
-  return textarea.value;
-}
 
-function createTask(text) {
-  var newTask = document.createElement("div");
-  newTask.innerText = text;
-  return newTask;
-}
 
-function addTaskToContainer(task) {
-  var taskContainer = document.getElementById("tasks-container");
-  taskContainer.appendChild(task);
-}
+
+
+
+// //Funcion que se ejecuta cuando termina de cargar
+// function main() {
+//   var addTaskButton = document.getElementById("add-task");
+//   addTaskButton.addEventListener("click",onClickAddTask);
+
+// function onClickAddTask(e) {
+//   e.preventDefault();
+//   var text = getTextAreaValue();
+//   if (text != "" && text.length <= 140) {
+//     var newTask = createTask(text);
+//     addTaskToContainer(newTask);
+//   } else {
+//     alert("Ingresa texto entre 0 y 140 caracteres");
+//   }
+// }
+
+// function getTextAreaValue() {
+//   var textarea = document.querySelector(".add-tasks textarea");
+//   return textarea.value;
+// }
+
+// function createTask(text) {
+//   var newTask = document.createElement("div");
+//   newTask.innerText = text;
+//   return newTask;
+// }
+
+// function addTaskToContainer(task) {
+//   var taskContainer = document.getElementById("tasks-container");
+//   taskContainer.appendChild(task);
+// }
